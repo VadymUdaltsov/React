73f1c119fc080747284e731634670563cb4b2ab4
Vadym Udaltsov <vadym.udaltsov@mail.ru>
2022-04-27 23:05:07 +0200

    added img and updated App.css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -36,7 +36,26 @@ const App = () => {
         </div>
       </nav>
       <div className='content'>
-        Main Content
+        <div>
+          <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' alt='img_bich' className='img-bich' />
+        </div>
+        <div>
+          <img src='https://d5nunyagcicgy.cloudfront.net/external_assets/hero_examples/hair_beach_v391182663/original.jpeg' alt='user' className='img-user' />
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            <div>
+              Post 1
+            </div>
+            <div>
+              Post 2
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

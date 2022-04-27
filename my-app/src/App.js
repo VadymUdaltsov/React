b7f2d2339d13ca06890c34171976872fb7b219b3
Vadym Udaltsov <vadym.udaltsov@mail.ru>
2022-04-27 22:32:01 +0200

    start New Project at App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
-import Header from './js_import/header';
-import Technologies from './js_import/technologies';
+/* import Header from './js_import/header';
+import Technologies from './js_import/technologies'; */
 
-const App = () => { /* разметка в App это компанента */
+/* const App = () => {  разметка в App это компанента 
   return (
     <div>
-      <Header /> {/* вызов разметки Header в App */}
-      <Technologies /> {/* Вызов разметки Technologies в App */}
+      <Header />  вызов разметки Header в App 
+      <Technologies />  Вызов разметки Technologies в App 
+    </div>
+  );
+} */
+
+const App = () => {
+  return (
+    <div>
+      
     </div>
   );
 }

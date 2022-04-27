58d8e27154a36f4d6e65b886cc4e759c19c9967e
Vadym Udaltsov <vadym.udaltsov@mail.ru>
2022-04-27 22:51:47 +0200

    added navbar and update App.css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,8 +14,30 @@ import Technologies from './js_import/technologies'; */
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/NBC_logo.svg/2000px-NBC_logo.svg.png" alt="logo" />
+      </header>
+      <nav className='nav'>
+        <div>
+          <a href='#s'>Profile</a>
+        </div>
+        <div>
+          <a href='#s'>Messages</a>
+        </div>
+        <div>
+          <a href='#s'>News</a>
+        </div>
+        <div>
+          <a href='#s'>Music</a>
+        </div>
+        <div>
+          <a href='#s'>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        Main Content
+      </div>
     </div>
   );
 }

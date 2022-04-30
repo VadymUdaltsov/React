bba848d2d064dc253e3733063587873ca0d460c6
Vadym Udaltsov <vadym.udaltsov@mail.ru>
2022-04-30 13:15:42 +0200

    Aded Header.module.css

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Header.css';
+import s from'./Header.module.css';
 
 const Header = () => {
     return (
-    <header className='header'>
+    <header className={s.header}>
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/NBC_logo.svg/2000px-NBC_logo.svg.png" alt="logo" />
     </header>
     ) 

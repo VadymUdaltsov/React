1cd1d1647cf6afe7a6d385312c1e073df6b4c31f
Vadym Udaltsov <vadym.udaltsov@mail.ru>
2022-04-30 12:35:16 +0200

    Added Content.module.css and Updated Content.jsx

--- my-app/src/components/Content.jsx
+++ my-app/src/components/Content.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import './Content.css';
+import './Content.module.css';
 
 const Content = () => {
     return (
-        <div className='content'>
+        <div className='Content_content__GgJDS'>
             <div>
-                <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' alt='img_bich' className='img-bich' />
+                <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' alt='img_bich' className='Content_img-bich__ubn4y' />
             </div>
             <div>
                 <img src='https://d5nunyagcicgy.cloudfront.net/external_assets/hero_examples/hair_beach_v391182663/original.jpeg' alt='user' className='img-user' />

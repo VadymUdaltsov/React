2d52c97d35a442e212aa1e480aeb4c5626cd9a08
Vadym Udaltsov <vadym.udaltsov@mail.ru>
2022-04-30 13:02:14 +0200

    Updated Content.jsx and Teoria.html

--- my-app/src/components/Content.jsx
+++ my-app/src/components/Content.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import './Content.module.css';
+import s from './Content.module.css';
+
+/* let s = {
+    'content': 'Content_content__GgJDS',
+    'img-bich': 'Content_img-bich__ubn4y'
+}; */
 
 const Content = () => {
     return (
-        <div className='Content_content__GgJDS'>
+        <div className={s.content}>
             <div>
-                <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' alt='img_bich' className='Content_img-bich__ubn4y' />
+                <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' alt='img_bich' className={s.img_bich} />
             </div>
             <div>
                 <img src='https://d5nunyagcicgy.cloudfront.net/external_assets/hero_examples/hair_beach_v391182663/original.jpeg' alt='user' className='img-user' />

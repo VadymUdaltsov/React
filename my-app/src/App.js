9cc6b7e4f47d8d0c38c1498fb5fa47ca7c1f5038
Vadym Udaltsov <vadym.udaltsov@mail.ru>
2022-04-30 11:53:34 +0200

    added Components: Header, Navbar, Content. Updated App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header.jsx';
+import Navbar from './components/Navbar.jsx';
+import Content from './components/Content.jsx';
 /* import Header from './js_import/header';
 import Technologies from './js_import/technologies'; */
 
@@ -15,48 +18,9 @@ import Technologies from './js_import/technologies'; */
 const App = () => {
   return (
     <div className='app-wrapper'>
-      <header className='header'>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/NBC_logo.svg/2000px-NBC_logo.svg.png" alt="logo" />
-      </header>
-      <nav className='nav'>
-        <div>
-          <a href='#s'>Profile</a>
-        </div>
-        <div>
-          <a href='#s'>Messages</a>
-        </div>
-        <div>
-          <a href='#s'>News</a>
-        </div>
-        <div>
-          <a href='#s'>Music</a>
-        </div>
-        <div>
-          <a href='#s'>Settings</a>
-        </div>
-      </nav>
-      <div className='content'>
-        <div>
-          <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' alt='img_bich' className='img-bich' />
-        </div>
-        <div>
-          <img src='https://d5nunyagcicgy.cloudfront.net/external_assets/hero_examples/hair_beach_v391182663/original.jpeg' alt='user' className='img-user' />
-        </div>
-        <div>
-          My posts
-          <div>
-            New post
-          </div>
-          <div>
-            <div>
-              Post 1
-            </div>
-            <div>
-              Post 2
-            </div>
-          </div>
-        </div>
-      </div>
+      <Header />
+      <Navbar />
+      <Content />
     </div>
   );
 }
---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+    <header className='header'>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/NBC_logo.svg/2000px-NBC_logo.svg.png" alt="logo" />
+    </header>
+    ) 
+    
+}
+
+export default Header;

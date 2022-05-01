b6f7a6ce527a71bda669b6648287d3ca8f0c59a4
Vadym Udaltsov <vadym.udaltsov@mail.ru>
2022-05-01 15:51:10 +0200

    Updated Dialogs

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
-/* import s from './Dialogs.module.css'; */
+import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialog_items}>
+                <div className={s.dialog}>
+                    Vadym
+                </div>
+                <div className={s.dialog}>
+                    Kirill
+                </div>
+                <div className={s.dialog}>
+                    Yasya
+                </div>
+                <div className={s.dialog}>
+                    Galina
+                </div>
+                <div className={s.dialog}>
+                    Sergey
+                </div>
+                <div className={s.dialog}>
+                    Kolya
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hi
+                </div>
+                <div className={s.message}>
+                    How are you?
+                </div>
+                <div className={s.message}>
+                    How is your progress?
+                </div>
+            </div>
         </div>
     )
 };

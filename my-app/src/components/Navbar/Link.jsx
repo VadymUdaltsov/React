18db99aafc5b29e51648a94fd5b385735554567a
Vadym Udaltsov <vadym.udaltsov@mail.ru>
2022-05-01 15:24:21 +0200

    Added Link.jsx. Updated Navbar

---
+++ my-app/src/components/Navbar/Link.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom'; /* обязательный импорт */
+
+/* NavLink замена обычной линке a, но лучше неё */
+/* Синтаксис <NavLink to='/link'></NavLink> */ 
+
+const ActiveLink = (props) => {
+    return (
+        <NavLink to={props.url} style={({ isActive }) => { 
+            return { color: isActive ? 'gold' : '' }
+        }}>{props.message}</NavLink>
+    )
+}; /* В React V-6 именно так добавляется стиль для активной NavLink, я вынес это в компаненту так как так легче её вставлять */
+
+export default ActiveLink;
--- my-app/src/components/Navbar/Navbar.jsx
+++ my-app/src/components/Navbar/Navbar.jsx
@@ -1,37 +1,28 @@
 import React from 'react';
-import s from './Navbar.module.css'
-import {NavLink} from 'react-router-dom'; /* обязательный импорт */
-
-const ActiveStyle = (props) => {
-    return (
-    <NavLink to={props.url} style={({ isActive }) => {
-        return { color: isActive ? 'gold' : '' }
-    }}>{props.message}</NavLink>
-    )
-    }; /* В React V-6 именно так добавляется стиль для активной NavLink, я вынес это в компаненту так как так легче её вставлять */
+import s from './Navbar.module.css';
+import ActiveLink from './Link';
 
 const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <ActiveStyle url="content" message="Profile"  />{/* NavLink замена обычной линке a, но лучше неё */}
-                {/* Синтаксис <NavLink to='/link'></NavLink> */}
+                <ActiveLink url="content" message="Profile" />
             </div>
             <div className={s.item}>
-                <ActiveStyle url="dialogs" message="Messages" />
+                <ActiveLink url="dialogs" message="Messages" />
             </div>
             <div className={s.item}>
-                <ActiveStyle url="news" message="News" /> { /* className={`${s.item} ${s.active}`} */ /* так мы пишем если два класса */ } 
+                <ActiveLink url="news" message="News" /> { /* className={`${s.item} ${s.active}`} */ /* так мы пишем если два класса */}
             </div>
             <div className={s.item}>
-                <ActiveStyle url="music" message="Music" />
+                <ActiveLink url="music" message="Music" />
             </div>
             <div className={s.item}>
-                <ActiveStyle url="setting" message="Setting" />
+                <ActiveLink url="setting" message="Setting" />
             </div>
         </nav>
-    ) 
-    
+    )
+
 }
 
 export default Navbar;

965f9cf7e69bbc92f895efc94a8f1c6da7060ee3
Vadym Udaltsov <vadym.udaltsov@mail.ru>
2022-05-01 15:14:44 +0200

    Added new Components at Navbar.jsx -- ActiveStyle.

--- my-app/src/components/Navbar/Navbar.jsx
+++ my-app/src/components/Navbar/Navbar.jsx
@@ -2,24 +2,32 @@ import React from 'react';
 import s from './Navbar.module.css'
 import {NavLink} from 'react-router-dom'; /* обязательный импорт */
 
+const ActiveStyle = (props) => {
+    return (
+    <NavLink to={props.url} style={({ isActive }) => {
+        return { color: isActive ? 'gold' : '' }
+    }}>{props.message}</NavLink>
+    )
+    }; /* В React V-6 именно так добавляется стиль для активной NavLink, я вынес это в компаненту так как так легче её вставлять */
+
 const Navbar = () => {
     return (
         <nav className={s.nav}>
-            <div>
-                <NavLink to='/content'>Profile</NavLink> {/* NavLink замена обычной линке a, но лучше неё */}
+            <div className={s.item}>
+                <ActiveStyle url="content" message="Profile"  />{/* NavLink замена обычной линке a, но лучше неё */}
                 {/* Синтаксис <NavLink to='/link'></NavLink> */}
             </div>
-            <div>
-                <NavLink to='/dialogs'>Messages</NavLink>
+            <div className={s.item}>
+                <ActiveStyle url="dialogs" message="Messages" />
             </div>
-            <div>
-                <NavLink to='/news'>News</NavLink> { /* className={`${s.item} ${s.active}`} */ /* так мы пишем если два класса */ } 
+            <div className={s.item}>
+                <ActiveStyle url="news" message="News" /> { /* className={`${s.item} ${s.active}`} */ /* так мы пишем если два класса */ } 
             </div>
-            <div>
-                <NavLink to='/music'>Music</NavLink>
+            <div className={s.item}>
+                <ActiveStyle url="music" message="Music" />
             </div>
-            <div>
-                <NavLink to='/settings'>Settings</NavLink>
+            <div className={s.item}>
+                <ActiveStyle url="setting" message="Setting" />
             </div>
         </nav>
     ) 
